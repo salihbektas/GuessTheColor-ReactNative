82e8a4090192fbb2a25b8a4f781d04dcd69f01f3
salihbektas <sbektas@gtu.edu.tr>
2022-10-16 02:42:35 +0300

    add choices generator

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
-  
+  const [choices, setChoices] = useState([
+    {color:"#", order:0},{color:"#", order:1},{color:"#", order:2}
+  ])
+
+
   function randomColorGenerator(){
     let r, g, b;
 
@@ -12,17 +16,65 @@ export default function App() {
     g = Math.floor(Math.random() * 256);
     b = Math.floor(Math.random() * 256);
 
-    return `rgb(${r}, ${g}, ${b})`;
+    return {r, g, b};
   }
-  
-  const [color, setColor] = useState(randomColorGenerator())
-  
+
+  function generateChoices(){
+    let {r, g, b} = randomColorGenerator()
+    let index = Math.floor(Math.random() * 3)
+
+    let color = "#";
+
+    [r,g,b].map(value => {
+      if(value < 16)
+        color += "0"
+
+      color += value.toString(16)
+    })
+
+    let newChices = [...choices];
+    newChices[0] = {color: color, order: index}
+    newChices[index].order = 0
+
+    for(let index = 1; index < 3; ++index){
+
+      [r,g,b].map(value => {
+        let min = value-100 >= 0 ? value-100 : 0
+        let max = value+100 <= 255 ? value+100 : 255
+
+        
+        let color = Math.floor(Math.random() * (max - min)) + min
+
+        if(color < 16)
+          newChices[index].color += "0"
+        
+        newChices[index].color += color.toString(16)
+      })
+    }
+
+
+
+    //let color1 = Math.floor(Math.random() * (max - min)) + min
+    //console.log(r)
+    //console.log(color1)
+
+    setChoices(newChices)
+
+  }
+
+  useEffect(()=>
+    console.log(choices)
+  ,[choices])
+
+  useEffect(()=> {
+    generateChoices()
+  },[])
   
   
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <View style={{height: "25%", width: "70%", backgroundColor: color, borderRadius: 16}} />
+      <View style={{height: "25%", width: "70%", borderRadius: 15, borderWidth:1, backgroundColor: choices[0].color}} />
       
 
       <View style={{borderWidth:1, padding: 4}}>
