294a14cc70a39a532a410d650b90005a8a5c191f
Salih Bektaş <sbektas@gtu.edu.tr>
2023-01-02 01:59:53 +0300

    add react navigation

---
+++ src/screens/Settings/Settings.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+
+export default function Settings(){
+
+    return(
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}} >
+            <Text>ayarlar sayfası</Text>
+        </View>
+    )
+}
